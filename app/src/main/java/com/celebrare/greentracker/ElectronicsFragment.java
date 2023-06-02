5585b702d8bf1e3db4dfdb1d845a37913a6d7cd6
ShubhGupta2004 <shubh.gupta2021@vitstudent.ac.in>
2023-06-03 04:04:38 +0530

    initiial

--- app/src/main/java/com/celebrare/greentracker/ElectronicsFragment.java
+++ app/src/main/java/com/celebrare/greentracker/ElectronicsFragment.java
@@ -13,6 +13,7 @@ public class ElectronicsFragment extends Fragment {
     private EditText editTVUsage;
     private EditText editFridgeUsage;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_electronics, container, false);
